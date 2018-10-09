c3f2d339410bb671f609bc1e902a4798a750ff45
Bruno Wakiyama <takebo@gmail.com>
2018-10-09 02:33:13 -0300

    changed directory, moved to components.

---
+++ src/components/Footer/Footer.js
@@ -0,0 +1,26 @@
+import React from 'react'
+import PropTypes from 'prop-types'
+import './Footer.css'
+
+const Footer = props => {
+  return (
+    <footer className="Footer">
+      <div className="copyright">{props.copyright}</div>
+      <div className="Pagination">
+        <a href="/">prev</a>
+        <a href="/">1</a>
+        <a href="/">2</a>
+        <a href="/">3</a>
+        <a href="/">4</a>
+        <a href="/">5</a>
+        <a href="/">next</a>
+      </div>
+    </footer>
+  )
+}
+
+Footer.propTypes = {
+  copyright: PropTypes.string
+}
+
+export default Footer
