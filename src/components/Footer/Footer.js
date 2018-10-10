056ee677d43c443f58e8a4d86ce0af4ddeffd005
Bruno Wakiyama <takebo@gmail.com>
2018-10-10 01:52:01 -0300

    replace pagination with paginator component.

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -2,19 +2,13 @@ import React from 'react'
 import PropTypes from 'prop-types'
 import './Footer.css'
 
+import Paginator from '../Paginator/Paginator'
+
 const Footer = props => {
   return (
     <footer className="Footer">
       <div className="copyright">{props.copyright}</div>
-      <div className="Pagination">
-        <a href="/">prev</a>
-        <a href="/">1</a>
-        <a href="/">2</a>
-        <a href="/">3</a>
-        <a href="/">4</a>
-        <a href="/">5</a>
-        <a href="/">next</a>
-      </div>
+      <Paginator />
     </footer>
   )
 }
