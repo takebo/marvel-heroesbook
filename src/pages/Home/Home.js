723667a81d5d1a4e6a68782acc811e800c115afd
Bruno Wakiyama <takebo@gmail.com>
2018-10-10 01:56:14 -0300

    removed unused object keys, created getHeroes function with data from config.js

--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -1,65 +1,34 @@
 import React, {Component} from 'react'
 import axios from 'axios'
+import {marvelApi as config} from '../../config/config'
 
 import './Home.css'
 
+import Footer from '../../components/Footer/Footer'
 import Heroes from '../../components/Heroes/Heroes'
 
 class Home extends Component {
-  constructor() {
-    super()
+  constructor(props) {
+    super(props)
     this.state = {
       heroes: [],
       params: {
         limit: 18,
-        offset: 0,
-        name: null, // Return only characters matching the specified full character name (e.g. Spider-Man)
-        nameStartsWith: null, // Return characters with names that begin with the specified string (e.g. Sp)
-        modifiedSince: null, // Return only characters which have been modified since the specified date
-        comics: null, // Number (accepts a comma-separated list of ids)
-        series: null, // Number (accepts a comma-separated list of ids)
-        events: null, // Number (accepts a comma-separated list of ids)
-        stories: null, // Number (accepts a comma-separated list of ids)
-        orderBy: 'name' // name, modified, -name, -modified
+        offset: 0
       }
+      // nameStartsWith: null, // Return characters with names that begin with the specified string (e.g. Sp)
+      // orderBy: 'name' // name, modified, -name, -modified
     }
   }
 
-  pagePrev = () => {
-    this.setState(prevState => ({
-      params: {
-        ...prevState.params,
-        offset: prevState.params.offset - 30
-      }
-    }))
-    console.log('prev: ', this.state.params.offset)
-
-    this.api(this.state.params)
-  }
-
-  pageNext = () => {
-    this.setState(prevState => ({
-      params: {
-        ...prevState.params,
-        offset: prevState.params.offset + 30
-      }
-    }))
-    console.log('next: ', this.state.params.offset)
-
-    this.api(this.state.params)
-  }
-
-  api(params) {
-    const apiUrl = 'http://gateway.marvel.com/v1/public/characters'
-    const publickey = '0ae171b5759a095e9a68bb5b446a9674'
-
+  getHeroes = params => {
     axios
-      .get(apiUrl, {
+      .get(config.baseUrl, {
         params: {
-          ts: 'takebo',
-          hash: '46c80334dd17064c5fe26fb8b536dce4',
-          apikey: publickey,
-          ...params
+          ...this.state.params,
+          ts: config.ts,
+          apikey: config.apikey,
+          hash: config.hash
         }
       })
       .then(res => {
@@ -73,11 +42,16 @@ class Home extends Component {
   }
 
   componentDidMount() {
-    this.api(this.state.params)
+    this.getHeroes()
   }
 
   render() {
-    return <Heroes data={this.state.heroes} />
+    return (
+      <>
+        <Heroes data={this.state.heroes} />
+        <Footer />
+      </>
+    )
   }
 }
 
