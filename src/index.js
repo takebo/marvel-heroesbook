4fcd3f8ba361ecdd5787a7c7bfe8dca1b0aac668
Bruno Wakiyama <takebo@gmail.com>
2018-10-11 04:22:17 -0300

    removed App.js and moved the code to index.js

--- src/index.js
+++ src/index.js
@@ -1,9 +1,33 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
+import {BrowserRouter as Router, Route, Switch} from 'react-router-dom'
+import {createStore} from 'redux'
+import {Provider} from 'react-redux'
+
+import Home from './pages/Home/Home'
+import Details from './pages/Details/Details'
+import Favorites from './pages/Favorites/Favorites'
+import NotFound from './pages/NotFound/NotFound'
+
 import './index.css'
-import App from './App'
 import * as serviceWorker from './serviceWorker'
 
-ReactDOM.render(<App />, document.getElementById('root'))
+import reducer from './store/reducer'
+
+const store = createStore(reducer)
+
+ReactDOM.render(
+  <Provider store={store}>
+    <Router>
+      <Switch>
+        <Route path="/" exact component={Home} />
+        <Route path="/hero/:id" component={Details} />
+        <Route path="/favorites" component={Favorites} />
+        <Route path="*" component={NotFound} />
+      </Switch>
+    </Router>
+  </Provider>,
+  document.getElementById('root')
+)
 
 serviceWorker.unregister()
