df82056e720d0ff7d000efc96a4d599137e2ab94
Bruno Wakiyama <takebo@gmail.com>
2018-10-11 04:24:15 -0300

    add not found page

--- src/pages/NotFound/NotFound.js
+++ src/pages/NotFound/NotFound.js
@@ -1,7 +1,22 @@
 import React from 'react'
+import {Link} from 'react-router-dom'
+import './NotFound.css'
+
+import Header from '../../components/Header/Header'
 
 const NotFound = () => {
-  return <h1>Page Not Found 404</h1>
+  return (
+    <>
+      <Header />
+      <div className="NotFound">
+        <h1 className="message error">404</h1>
+        <p className="message warning">No hero ever wonders this area.</p>
+        <Link to="/" className="message">
+          <div>escape the void</div>
+        </Link>
+      </div>
+    </>
+  )
 }
 
 export default NotFound
